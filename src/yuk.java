6642517fa69e38538b208c7a8922f55754282faa
jae yuk <utopia2134@gmail.com>
2025-10-22 13:05:40 +0900

    dk

---
+++ src/yuk.java
@@ -0,0 +1,9 @@
+public class yuk {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("감사해요");
+        System.out.println("잘있어요");
+        System.out.println("다시 만나요");
+
+    }
+}
